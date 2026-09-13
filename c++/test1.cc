b8836050ea2882a4c2c4dd5908c8dfda71043672
anton praveen <fernandowapn@gmail.com>
2026-09-13 11:00:53 +0530

    fix: created gitignore file

--- c++/test1.cc
+++ c++/test1.cc
@@ -6,12 +6,17 @@ using namespace std;
 int main(){
     string name = "Anton";
     string response;
+    string contact;
 
     cout << "Hello " << name << endl;
     cout << "how are you?" << "\n";
 
     getline(cin, response);
     cout << "You said: " << response << endl;
+
+    cout << "What is your phone number?" << "\n";
+    getline(cin, contact);
+    cout << "Your phone number: " << contact << endl;
 
     return 0;
 }
